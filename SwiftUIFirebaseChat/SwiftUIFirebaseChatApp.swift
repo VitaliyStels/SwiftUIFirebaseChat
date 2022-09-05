3989d4adf725caad98c0a00aa8a670579ebcba7b
VitaliyStels <38788206+VitaliyStels@users.noreply.github.com>
2022-09-05 07:43:12 +0300

    Initial Commit

---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Виталий on 05.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
